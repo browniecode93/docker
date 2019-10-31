db671de462289ae90e718212baced90bcd5be41d
browniecode93 <53164544+browniecode93@users.noreply.github.com>
2019-10-31 18:15:00 +0330

    Update SmsProviderController.php

--- sms_provider/src/Controller/SmsProviderController.php
+++ sms_provider/src/Controller/SmsProviderController.php
@@ -5,7 +5,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-// ...
 
 class SmsProviderController extends AbstractController
 {
