c0c76c88d9c23a84baff38c5d9466421158e4f57
Farzaneh Dejhband <farzaneh@Farzanehs-MacBook-Pro.local>
2019-10-31 17:46:18 +0330

    add sms provider

---
+++ sms_provider/src/Controller/SmsProviderController.php
@@ -0,0 +1,24 @@
+<?php
+// src/Controller/SmsApiController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+// ...
+
+class SmsProviderController extends AbstractController
+{
+    public function send_sms_to_users(Request $request)
+    {
+        if(round(rand(0,1), 0,PHP_ROUND_HALF_EVEN)) {
+            $response = new Response("Sending fail", 500);
+            return $response->send();
+        }
+        $response = new Response("Sending successfully", 200);
+        return $response->send();
+    }
+
+
+}
